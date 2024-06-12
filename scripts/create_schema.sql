b772350317533c4d879bfcab1be2ae6481c8788d
Martijn Pronk <martijn.pronk@amis.nl>
2024-06-12 09:47:36 +0200

    further refinement and automation of pod

--- scripts/create_schema.sql
+++ scripts/create_schema.sql
@@ -1,10 +1,12 @@
-create user app_test identified by Welkom01x123 account unlock;
-grant soda_app,connect, resource to app_test;
-alter user app_test quota unlimited on data;
+create user apptest identified by Welkom01x123 account unlock;
+grant soda_app,connect, resource to apptest;
+alter user apptest quota unlimited on data;
 
 begin
-ords.enable_schema(p_schema => 'APP_TEST');
+ords.enable_schema(p_schema => 'APPTEST');
 end;
 /
 
--- mongodb://app_test:Welkom01x123@localhost:27017/app_test?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false&loadBalanced=true
+exit
+
+-- mongodb://apptest:Welkom01x123@localhost:27017/apptest?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false&loadBalanced=true
